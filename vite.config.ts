2574c314da86fa99ef97f4e13fefa65622784248
Emmanuel <asamoahe304@gmail.com>
2024-07-19 16:18:57 +0000

    refined lib names and configs

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: resolve(__dirname, 'src', 'index.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es', 'cjs'],
       fileName: (ext) => `index.${ext}.js`
     },
